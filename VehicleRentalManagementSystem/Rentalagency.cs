dc7d0bead2f6fa672e28c51d328771f6af108ba1
arfeena <ajamal7816>
2024-07-19 01:39:19 +0530

    Rentalagency

---
+++ VehicleRentalManagementSystem/Rentalagency.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RentalAgency
+{
+    private List<Vehicle> fleet = new List<Vehicle>();
+    private decimal totalRevenue;
+
+    public decimal TotalRevenue
+    {
+        get { return totalRevenue; }
+    }
+
+    public void AddVehicle(Vehicle vehicle)
+    {
+        fleet.Add(vehicle);
+        Console.WriteLine("Vehicle added to fleet.");
+    }
+
+    public void RemoveVehicle(Vehicle vehicle)
+    {
+        fleet.Remove(vehicle);
+        Console.WriteLine("Vehicle removed from fleet.");
+    }
+
+    public void RentVehicle(Vehicle vehicle, int days)
+    {
+        if (fleet.Contains(vehicle))
+        {
+            fleet.Remove(vehicle);
+            totalRevenue += vehicle.RentalPrice * days;
+            Console.WriteLine($"Vehicle rented for {days} days. Total revenue now: {totalRevenue}");
+        }
+        else
+        {
+            Console.WriteLine("Vehicle not available for rent.");
+        }
+    }
+
+    public void DisplayFleet()
+    {
+        foreach (var vehicle in fleet)
+        {
+            vehicle.DisplayDetails();
+            Console.WriteLine();
+        }
+    }
+}
